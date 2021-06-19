22142f6b4cc3b730b6022c71dd445cbc4bde3888
Alejandro G <kleon0313@gmail.com>
2021-06-19 11:29:50 -0500

    1759 solution

---
+++ Solutions/URI_1759.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Solutions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alex
+ */
+public class URI_1759 {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int N = read.nextInt();
+
+        while (N-- > 0) {
+            System.out.print("Ho");
+            if (N > 0) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println("!");
+
+    }
+}
